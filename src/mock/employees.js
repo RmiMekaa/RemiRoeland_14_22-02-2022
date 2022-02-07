2feada16d715f3530b5bbe5bad0bfaca1222365d
Rémi <roeland.remi@yahoo.fr>
2022-02-07 19:03:19 +0100

    Create files for utils data and mock data

---
+++ src/mock/employees.js
@@ -0,0 +1,58 @@
+export const employees = [
+  {
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+    birthdate: '17/02/1939',
+    startDate: '01/01/2022',
+    department: 'Marketing',
+    street: 'Wayne Manor',
+    city: 'Gotham City',
+    state: 'Chicago',
+    zipCode: '60007',
+  },
+  {
+    firstName: 'Clark',
+    lastName: ' Kent',
+    birthdate: '18/06/1933',
+    startDate: '02/02/2022',
+    department: 'Sales',
+    street: 'Krypto Street',
+    city: 'Metropolis',
+    state: 'Illinois',
+    zipCode: '60601',
+  },
+  {
+    firstName: 'Peter',
+    lastName: 'Parker',
+    birthdate: '05/08/1962',
+    startDate: '03/03/2022',
+    department: 'Legal',
+    street: 'Spider Street',
+    city: 'New York City',
+    state: 'New York',
+    zipCode: '10001',
+  },
+  {
+    firstName: 'Al',
+    lastName: 'Simons',
+    birthdate: '01/05/1992',
+    startDate: '04/04/2022',
+    department: 'Human Resources',
+    street: 'Hell Street',
+    city: 'Rat City',
+    state: 'Pennsylvania',
+    zipCode: '15042',
+  }
+]
+
+export const headers = [
+  "First Name",
+  "Last Name",
+  "Date of Birth",
+  "Start Date",
+  "Department",
+  "Street",
+  "City",
+  "State",
+  "Zip Code"
+]
