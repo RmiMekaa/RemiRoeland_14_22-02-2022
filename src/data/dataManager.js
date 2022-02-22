64c8ce9a5d21272e0624b3acc96eebcb5fe12d70
Rémi <roeland.remi@yahoo.fr>
2022-02-22 15:13:06 +0100

    Handling data

---
+++ src/data/dataManager.js
@@ -0,0 +1,11 @@
+import { EMPLOYEES } from "./data"
+
+/**
+ * Add the employee to the database and save it in the local Storage
+ * @param   {Object}  employee  New employee to add
+ * @return  {void}
+ */
+export function addEmployee(employee) {
+  EMPLOYEES.push(employee);
+  localStorage.setItem('employees', JSON.stringify(EMPLOYEES))
+}
