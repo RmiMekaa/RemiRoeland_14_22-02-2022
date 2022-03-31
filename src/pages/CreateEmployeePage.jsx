3813a987e1aea86f5305f2b499112cd92e015afe
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-03-31 15:20:53 +0200

    Replace old modal by newest

--- src/pages/CreateEmployeePage.jsx
+++ src/pages/CreateEmployeePage.jsx
@@ -1,37 +1,19 @@
 import React, { useState } from "react";
 import Form from "../components/Form/Form";
-import CustomModal from "../components/CustomModal/CustomModal";
+import { Modal } from "../components/Modal/Modal"
 
 /**
- * React component for the home page
+ * Component for the create employee page
  * @component
  */
 export default function CreateEmployeePage() {
   const [displayModal, toggleDisplayModal] = useState(false);
 
-  const customModalStyle = {
-    box: {
-      backgroundColor: '#3f3d4b',
-      border: '#646167 solid 3px',
-      color: '#fcf1d5',
-      padding: '2rem 4rem',
-    },
-    button: {
-      backgroundColor: '#27262f',
-      color: '#fcf1d5',
-    }
-  }
-
   return (
     <main className="homepage">
       <h1>Create employee</h1>
-      <Form toggleModal={toggleDisplayModal} />
-      <CustomModal
-        displayModal={displayModal}
-        onClose={() => toggleDisplayModal(false)}
-        customStyle={customModalStyle}
-        msg="Employee created !"
-      />
+      <Form {...{ toggleDisplayModal }} />
+      <Modal {...{ displayModal, toggleDisplayModal }} />
     </main>
   )
 }
