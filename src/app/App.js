c8105d74006e7586fc19aff9235c0030f678da2c
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-04-18 15:48:29 +0200

    feat: Default path "/" now redirect to employees page

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import CreateEmployeePage from "../pages/CreateEmployeePage";
 import EmployeesPage from "../pages/EmployeesPage";
 import Header from '../components/Header/Header';
@@ -8,7 +8,8 @@ export default function App() {
     <Router>
       <Header />
       <Routes>
-        <Route exact path="/" element={<CreateEmployeePage />} />
+        <Route exact path="/" element={<Navigate to="/Employees" />} />
+        <Route path="/Create-employee" element={<CreateEmployeePage />} />
         <Route path="/Employees" element={<EmployeesPage />} />
       </Routes>
     </Router>
