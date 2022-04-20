9af4e64ee84182c7435f7666264cf6986f55c9cf
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-04-20 14:57:22 +0200

    refactor: Merge App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App.js';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import CreateEmployeePage from "./pages/CreateEmployeePage";
+import EmployeesPage from "./pages/EmployeesPage";
+import Header from './components/Header';
 import './sass/main.scss';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/Employees" />} />
+        <Route path="/Create-employee" element={<CreateEmployeePage />} />
+        <Route path="/Employees" element={<EmployeesPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
