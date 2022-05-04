2c8e918cc62b5fcc2c2eb85550e9649feaa5771f
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-05-04 16:17:01 +0200

    refactor: Replace BrowserRouter by HashRouter to handles routing in github pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; //Use HashRouter instead of BrowserRouter to handle routing in github page.
 import { CreateEmployeePage } from "./pages/CreateEmployeePage";
 import { EmployeesPage } from "./pages/EmployeesPage";
 import { Header } from './components/Header';
